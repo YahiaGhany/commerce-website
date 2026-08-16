f5f6c4863eff81db9e0e877ad44da3aaefcd24a5
Alpha X32 <zephito1103@gmail.com>
2026-08-16 13:43:21 +0100

    Update secondary image vert fonce and bump cache

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ function openProduct(id) {
   
   if (secondaryFilename) {
     const mainImgUrl = optimizeImg(currentProduct.img);
-    const secondaryImgUrl = optimizeImg(`assets/images_secondaires/${secondaryFilename}?v=2`);
+    const secondaryImgUrl = optimizeImg(`assets/images_secondaires/${secondaryFilename}?v=3`);
     
     document.getElementById('pVisual').innerHTML = `
       <div class="product-slider" id="productSlider">
